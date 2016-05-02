c77485a04675a01a6da06e326cd03875e48c0f28
Hugo <hugo.hornquist@gmail.com>
2016-05-02 23:14:36 +0200

    shortened code for rainbow-paren

--- rainbow-paren.scm
+++ rainbow-paren.scm
@@ -2,37 +2,26 @@
   (define str-list (string->list str))
   (define (inner remaining depth escaped in-string)
     (if (not (null? remaining))
-      (let ((ch (car remaining)))
-        (if escaped
-          (begin
+      (let ((ch (car remaining))
+            (default-print (lambda (ch)
+                             (display ch)
+                             (inner (cdr remaining)
+                                    depth
+                                    (if (eqv? ch #\\) #t #f)
+                                    (if (eqv? ch #\") (not in-string) in-string))))
+            (fancy-print (lambda (ch depth-mod get-next)
+                           (display (color-char (get-color (depth-mod depth)) 'none ch))
+                           (inner (cdr remaining) (get-next depth) #f in-string))))
+        (cond
+          (escaped
             (display ch)
             (inner (cdr remaining) depth #f in-string))
-          (cond
-            ((eqv? ch #\\)
-             (display ch)
-             (inner (cdr remaining) depth #t in-string))
-            ((eqv? ch #\")
-             (display ch)
-             (if in-string
-               (inner (cdr remaining) depth #f #f)
-               (inner (cdr remaining) depth #f #t)))
-            ((eqv? ch #\()
-             (if in-string
-               (begin
-                 (display ch)
-                 (inner (cdr remaining) depth #f in-string))
-               (begin
-                 (display (color-char (get-color depth) 'none ch))
-                 (inner (cdr remaining) (inc depth) #f in-string))))
-            ((eqv? ch #\))
-             (if in-string
-               (begin
-                 (display ch)
-                 (inner (cdr remaining) depth #f in-string))
-               (begin
-                 (display (color-char (get-color (dec depth)) 'none ch))
-                 (inner (cdr remaining) (dec depth) #f in-string))))
-            (else
-              (display ch)
-              (inner (cdr remaining) depth #f in-string)))))))
+          ((or (eqv? ch #\() (eqv? ch #\)))
+           (if in-string
+             (default-print ch)
+             (if (eqv? ch #\))
+               (fancy-print ch dec dec)
+               (fancy-print ch (lambda (x) x) inc))))
+          (else
+            (default-print ch))))))
   (inner str-list 0 #f #f))
